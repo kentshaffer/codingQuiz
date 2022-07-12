706923768fda73903ee8ad8f3f23f665d32b895c
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-07-12 14:48:47 -0700

    js files added, quiz questions added, application google font style linked

---
+++ assets/js/quiz.js
@@ -0,0 +1,31 @@
+var quizContent = [
+    {
+      question: "Which of the following is a commonly used Javascript loop type?:",
+      possibleAnswers: ["while loop", "alert loop", "string loop", "boolean loop"],
+      correctAnswer: "while loop"
+    },
+
+    {
+      question: "What function should be used to select a random array item?:",
+      possibleAnswers: ["math.floor()", "random.number()", "math.random()", "console.log(random)"],
+      correctAnswer: "math.random()"
+    },
+
+    {
+      question: "Which of the following coding languages creates user functionality within web applications?:",
+      possibleAnswers: ["HTML", "Javascript", "CSS", "none of the above"],
+      correctAnswer: "Javascript"
+    },
+
+    {
+      question: "Which of the following git commands sends changes to a repository?:",
+      possibleAnswers: ["git push", "git commit", "git pull origin main", "git add"],
+      correctAnswer: "git push"
+    },
+
+    {
+      question: "Where is the best place on the HTML page to link the Javascript?:",
+      possibleAnswers: ["top of page", "bottom of page", "doesn't matter", "all of the above"],
+      correctAnswer: "bottom of page"
+    },
+];
