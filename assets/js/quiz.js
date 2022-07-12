6aad9405ff1237f5ecf669abebf90d3df79dab2c
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-07-12 15:40:42 -0700

    js fuctions layed out

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,4 @@
-var quizContent = [
+let quizContent = [
     {
       question: "Which of the following is a commonly used Javascript loop type?:",
       possibleAnswers: ["while loop", "alert loop", "string loop", "boolean loop"],
@@ -29,3 +29,31 @@ var quizContent = [
       correctAnswer: "bottom of page"
     },
 ];
+
+let correctSound = new Audio('assets/sfx/correct.wav');
+let incorrectSound = new Audio('assets/sfx/incorrect.wav');
+
+let selectedQuestion = 0;
+let timer = questions.length * 15;
+let seconds;
+
+let quizQuestions = documet.getElementById('quizQuestions');
+let timeLeft = documet.getElementById('timeLeft');
+let answers = documet.getElementById('answers');
+let postScore = documet.getElementById('post');
+let startQuiz = documet.getElementById('startQuiz');
+let playerNname = documet.getElementById('name');
+let quizResults = documet.getElementById('results');
+
+//function for starting app
+
+//function for question selection
+
+//function to listen for question selection click
+
+//function to end quiz when all questions have been asked
+
+//function for time clock to end quiz if 0 
+
+//fuction to record to leaderboard
+
